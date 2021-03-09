105dbe85061aa5153e3a738ddf40e75d3faaa28d
keka3avr <kekazavr228@gmail.com>
2021-03-09 12:12:31 +1100

    Homework 1.3 added

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,41 @@
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    "use strict";
+    let paramNames = ['percent', 'contribution', 'amount'];
+    let parameters = [percent, contribution, amount];
+
+    if (isNaN(Date.parse(date)) == true) {
+        return "Введите правильную дату";
+    }
+
+    for (let i = 0; i < parameters.length; i++) {
+        if (!parameters[i]) {
+            return `Параметр ${paramNames[i]} не заполнен!`; 
+        }        
+        if (isNaN(parseInt(parameters[i])) == true) {
+            return `Параметр ${paramNames[i]} содержит неправильное значение ${parameters[i]}`;
+        }
+    }
+
+    let body = amount - contribution;
+    let monthPercent = percent / 1200;
+    let totalMonths = monthDiff(date, new Date);
+    console.log(totalMonths);
+
+    let monthPayment = body * (monthPercent + monthPercent / ((Math.pow(1 + monthPercent, totalMonths) - 1)));
+    return monthPayment * totalMonths;
+}
+
+function monthDiff(d1, d2) {
+    let months;
+    months = (d2.getFullYear() - d1.getFullYear()) * 12;
+    months -= d1.getMonth() + 1;
+    months += d2.getMonth();
+    return months;
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    if (name == "") {
+        name = 'Аноним';
+    }
+    return `Привет, мир! Меня зовут ${name}`;
 }
