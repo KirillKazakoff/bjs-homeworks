fe50abf3f511c1afc50995ce0eeca7dcd17735f8
keka3avr <kekazavr228@gmail.com>
2021-03-13 13:53:32 +1100

    Homework 2.2 added

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,28 @@
 //String.prototype.isPalindrome - для задачи №1
+String.prototype.isPalindrome = function() {    
+    this.reverseString = reverseString;
+    if(this.reverseString = this.split(' ').join('').toLowerCase()) {
+        return true;
+    }
+    else return false;
+}
+
+function reverseString() {
+    return this.split(' ').reverse().join('').toLowerCase();
+}
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    let roundedAverage = Math.round(marks.reduce((total,value) => total + value) / marks.length);
+    return roundedAverage;
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+   let now = +new Date;
+   let personBirth = +new Date(birthday);
+   let diff = now - personBirth;
+   let age = diff / 31600400000;
+   if (age >= 18) {
+        return true;
+   }
+   else return false;
 }
