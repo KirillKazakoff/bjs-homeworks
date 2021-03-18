736f174585cdaff9e5d334f5d53142489d7e3ade
keka3avr <kekazavr228@gmail.com>
2021-03-19 10:51:38 +1100

    Homework 3.1 fixed

--- 3.1-exceptions-closure/task.js
+++ 3.1-exceptions-closure/task.js
@@ -19,18 +19,21 @@ function validateCount(value) {
 class Triangle {
     constructor(a, b, c) {
         if (a + b < c || a + c < b || b + c < a) {
-            throw new Error("Треугольник с такими сторонами не существует!");
+            throw new Error("Треугольник с такими сторонами не существует");
         }
         this.aSide = a;
         this.bSide = b;
         this.cSide = c;
     }
-    getPerimeter = () => this.aSide + this.bSide + this.cSide;
+    getPerimeter() {
+        return this.aSide + this.bSide + this.cSide;
+    }
     
     getArea() {
         let pHalf = this.getPerimeter() / 2;
-        return Math.sqrt(pHalf * (pHalf - this.aSide) * (pHalf - this.bSide) * (pHalf - this.cSide));
+        return Math.sqrt(pHalf * (pHalf - this.aSide) * (pHalf - this.bSide) * (pHalf - this.cSide)).toFixed(3);
     }
+
 }
 
 
@@ -39,17 +42,88 @@ function getTriangle(a, b, c) {
         return new Triangle(a, b, c);
     }
     catch(error) {  
-        return {
-                
+        return {  
             getArea() {
-                return "Ошибка, треугольник не существует!"
+                return "Ошибка! Треугольник не существует"
             },
             getPerimeter() {
-                return "Ошибка, треугольник не существует";
+                return "Ошибка! Треугольник не существует";
             }
             
         }
     }
 }
 
-let firstTriangle = new Triangle(10, 12, 7);
+
+let firstTriangle = new Triangle(6, 10, 15);
+
+// callbacks
+const fakeNames = { 
+    0: "Petya",
+    1: "Kolya",
+    2: "Sasha",
+    length: "3",
+    forEach: function(callback) {
+       for (key in this) {
+           if (key !== 'length') {
+               callback(this[key])
+           }
+           
+       }   
+    }
+}
+
+//reduce
+
+const students = [
+    {
+        name: "joba",
+        age: "22"
+    },
+    {
+        name: "joba",
+        age: "23"
+    },
+    {
+        name: "joba",
+        age: "24"
+    },
+    {
+        name: "joba",
+        age: "23"
+    },
+    {
+        name: "joba",
+        age: "22"
+    }
+]
+
+const agesStudents = students.reduce((ages, student) => { //ages - object, student - current value(object)
+    const currentAge = student.age;
+
+    if (!ages[currentAge]) currentAge = [];
+
+    ages[currentAge].push(student.name);
+
+    return ages;  
+}, {}) //{} - initial empty object
+
+// "dsf".startsWith('d') = true
+
+let memory = '';
+
+students.some(student => {
+    let memory = memory + student.name;
+
+    if (memory.length > 50) return true;
+
+    console.log(student.name);
+    return;
+})
+
+
+// можно использовать для прерывания forEach?
+
+// leetcode - задачи для js
+
+// мемонизация? меморизация? мемоизация?
