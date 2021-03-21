4c6b75d4879077aa297dd45ae162e17155c325b6
keka3avr <kekazavr228@gmail.com>
2021-03-21 15:54:32 +1100

    Homework 3.2 fixed

--- 3.2-array-proto-hocs/functions.js
+++ 3.2-array-proto-hocs/functions.js
@@ -3,26 +3,26 @@
 console.clear();
 const weapons = [new Knife(), new Staff(), new Axe(), new StormStaff(), new LongBow(), new Bow()];
 
-function getNames(array) {
-    return array.reduce((names, item) => {
+function getNames() {
+    return weapons.reduce((names, item) => {
        names.push(item.name);
        return names;
     }, [])
 }
 
-function getCountReliableWeapons(durability, array) {
-    return array.reduce((count, weapon) => {
+function getCountReliableWeapons(durability) {
+    return weapons.reduce((count, weapon) => {
         if (weapon.durability > durability) count++;
         return count;
     }, 0);
 }
 
-function hasReliableWeapons(durability, array) {
-    return getCountReliableWeapons(durability, array) > 0;
+function hasReliableWeapons(durability) {
+    return getCountReliableWeapons(durability) > 0;
 }
 
-function getReliableWeaponsName(durability, array) {
-    return array.reduce((namesArray, weapon) => {
+function getReliableWeaponsNames(durability) {
+    return weapons.reduce((namesArray, weapon) => {
         if (weapon.durability > durability) {
             namesArray.push(weapon.name);
         }
@@ -30,14 +30,14 @@ function getReliableWeaponsName(durability, array) {
     }, [])
 }
 
-function getTotalDamage(array) {
-    return array.reduce((total, weapon) => total + weapon.attack, 0);
+function getTotalDamage() {
+    return weapons.reduce((total, weapon) => total + weapon.attack, 0);
 }
 
 function getValuestCountToSumValues(array, sumValue) {
-    let sortedArray = array.sort(compareNumbers);
+    // let sortedArray = array.sort(compareNumbers);
     let sum = 0;
-    return sortedArray.reduce((total, elem) => {
+    return array.reduce((total, elem) => {
         if (sum < sumValue) {
             sum = sum + elem;
             total = total + 1;
@@ -46,9 +46,9 @@ function getValuestCountToSumValues(array, sumValue) {
     }, 0)
 }
 
-function compareNumbers(a, b) {
-    return b - a;
-}
+// function compareNumbers(a, b) {
+//     return b - a;
+// }
 
 function sleep(milliseconds) {
     let e = new Date().getTime() + milliseconds;
@@ -79,6 +79,7 @@ function memorize(fn, limit) {
                memory.splice(0,1);
            }
        }
+       else result = result.result;
        return result;
    }
 }
